a986a9035df4ecd1d10d5295e810e654249a1dc7
Federik Codelas <icodeclean@gmail.com>
2014-01-18 03:22:49 -0500

    Ex 3 extra credit

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,28 @@
+# Intention
 puts "I will now count my chickens:"
-
+# Calculate Hens
 puts "Hens", 25 + 30 /6
+# Calculate Roosters
 puts "Roosters", 100 - 25 * 3 % 4
-
+# More Intentions
 puts "now I will count the eggs:"
-
+# Calculate Eggs
 puts  3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# Question
 puts " Is it true that 3 + 2 < 5 - 7?"
-
+# Answer via Boolean
 puts 3 + 2 < 5 - 7 
-
+# More Q and A
 puts "What is 3 + 2?", 3 + 2
+# More Q and A
 puts "What is 5 - 7?", 5 - 7
-
+# Duh!
 puts "Oh, that's why it's false."
-
+# The Hard Way is the Only Way!
 puts "How about some more."
-
+# More Q and A
 puts "Is it greater?", 5 > -2
+# and more
 puts "Is it greater or equal?", 5 >= -2
+# and more
 puts "Is it less or equal?", 5 <= -2
