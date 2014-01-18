58c9ea53e22fd5878eccd4d4953d88011ea927e3
Federik Codelas <icodeclean@gmail.com>
2014-01-18 14:16:30 -0500

    ltp assignment 1

---
+++ ltp1.rb
@@ -0,0 +1,8 @@
+puts "Hours in a year"
+puts 365 * 24
+puts "Minutes in a decade"
+puts (10 * 365 * 24 * 60) + ((2 * 24) + (24/2)) * 60
+puts "Age in seconds"
+puts ((32/4)*24*60*60) + (32*365*24*60*60)
+puts "Authors age in years. He was joking; he said he was 1,025,000,000 seconds old."
+puts 1025000000 / 60 / 60 / 24 / 365 / 10
