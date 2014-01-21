6e7f9c9547e59f53bc05a083d274583e0ff8f16d
Federik Codelas <icodeclean@gmail.com>
2014-01-20 23:35:19 -0500

    Exercise 5 credit2

--- ex5.rb
+++ ex5.rb
@@ -5,6 +5,8 @@ weight = 180 # ibs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+animal1 = 'koala'
+animal2 = 'monkey'
 
 puts "Let's talk about %s" % name
 puts "He's %d inches tall." % height
@@ -12,6 +14,7 @@ puts "He's %d pounds heavy." % weight
 puts "Actually, that's not too heavy."
 puts "He's got %s eyes and %s hair" % [eyes, hair]
 puts "His teeth are usually %s depending on the coffee." % teeth
+puts "He owns a %s and a %s." %[animal1, animal2]
 
 # this line is tricky, try to grt it exactly right
 puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
