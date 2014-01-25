08b9b1d96819e35ff4c9ae023a84cff416f48b65
Federik Codelas <icodeclean@gmail.com>
2014-01-25 01:20:39 -0500

    Exercise 9

---
+++ ex9.rb
@@ -0,0 +1,18 @@
+# Here's some new strnage stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nHun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With the PARAGRAPH THING
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
+
+__END__
+\n creates a newline in a string
+PARAGRAPH allows you to write however you want.
