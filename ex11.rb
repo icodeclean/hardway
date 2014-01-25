4d803af8b2ab1e085add2ca3e9442557944a05a9
Federik Codelas <icodeclean@gmail.com>
2014-01-25 03:21:26 -0500

    Exercise 11

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy"
+
+__END__
+Is () needed at the end of chomp
