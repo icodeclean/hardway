7818285c5a67a07dced2c39c46cb88cfdeafa0f2
Federik Codelas <icodeclean@gmail.com>
2014-01-25 04:58:41 -0500

    Exercise 15 with ammendments

--- ex15.rb
+++ ex15.rb
@@ -8,6 +8,7 @@ txt = File.open(filename)
 puts "Here's your file: #{filename}"
 # display txt
 puts txt.read()
+txt.close()
 
 puts "I'll also ask you to type it again:"
 print prompt
@@ -15,5 +16,11 @@ print prompt
 file_again = STDIN.gets.chomp()
 # open file and assign to txt_aagin
 txt_again = File.open(file_again)
+txt_again.close()
 
 puts txt_again.read()
+
+__END__
+- File.open(x)  to open a file
+- y.read()   to read the contents of the file
+- .close() to close a file after reading it.
