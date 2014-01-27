11a26a432af4affd1f7a00d3f4b1d243f57d2ea7
Federik Codelas <icodeclean@gmail.com>
2014-01-27 03:11:57 -0500

    Exercise 22

---
+++ ex22.rb
@@ -0,0 +1,23 @@
+'   Apostrophe      #Used for Strings. Also called single quotes
+"   Double quote    #Used for strings
+.   Period          #Used to run a method to an object. Also used to set a float
+,   Comma           #Used to seperate items in defining varibles, passing arguments, format sequences etc.
+*   Astericks       #Used to multiply and used in front of args for unpacking arguments in a method
++   Plus            #Used to add and for concatenation
+-   Hyphen          #Used to subtract
+/   Forward Slash   #Used for division. Also used in file paths
+\   Back Slash      #Used to excape a character. Also called a Virgule
+|   Pipe            #Used for the "OR" logic operation when used twice ||. Also called vertical bar.
+&   Ampersand       #Used for the "AND" logic operation when used twice &&
+!   Exclamation     #Used for the "NOT" logical operation when used in front of something.
+<   Angle Brackets  #Used as "less than". Also >= "less than or equal to". Used in  Doc String
+>   Angle Brackets  #Used as "greater than" in logical comparisons. And also >= "greater than or equal to"
+#   hash            #Used for comments. Also calle dpound and octothorpe
+=   Equal Sign      #Used to set a variable or compare items in a string when used twice. ==
+%   Percent         #Used in format sequences
+[]  Brackets        #Used in format sequences, arrays to encapsulate items
+{}  Braces          #Used in String interpolation. Also called Curly brackets
+()  Parentheses     #Used in method definitions and calling. Also used in Conditional statements. 
+_   Underscore      #Used in naming convention in the place of spaces.
+?   Question Mark   #Used in some built in methods like File.exist?
+:   Colon           #Used in some built in methods
